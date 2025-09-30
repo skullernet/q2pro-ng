0dab0ef394e212edc664593440daf25547539c12
Andrey Nazarov <skuller32@gmail.com>
2025-09-30 17:55:25 +0300

    Update project link in console.

--- src/common/common.c
+++ src/common/common.c
@@ -1001,7 +1001,7 @@ void Qcommon_Init(int argc, char **argv)
 
     Com_Printf("====== " PRODUCT " initialized ======\n\n");
     Com_NPrintf(APPLICATION " " VERSION ", " __DATE__ "\n");
-    Com_Printf("https://github.com/skullernet/q2pro\n\n");
+    Com_Printf("https://github.com/skullernet/q2pro-ng\n\n");
     Com_DPrintf("Compiled features: %s\n", Com_GetFeatures());
 
     time(&com_startTime);
